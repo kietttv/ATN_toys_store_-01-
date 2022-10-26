b66992153831502284dfe4506830cdd5cf04d32b
kietttv <tvtkiet2002@gmail.com>
2022-10-26 12:08:10 +0700

    new and best products

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 include_once("header.php");
 $sqlProduct = "SELECT id, name, suplier_id, shop_id, category_id, import_price, sell_price, quantitty, date, discription, image, status
                 FROM public.product WHERE status = 'Available' ORDER BY sell_price DESC LIMIT 3";
-$reProduct = pg_query($conn, $sqlProduct)
+$reProduct = pg_query($conn, $sqlProduct);
+
+$sqlNewProduct = "SELECT id, name, suplier_id, shop_id, category_id, import_price, sell_price, quantitty, date, discription, image, status
+FROM public.product WHERE status = 'Available' ORDER BY DATE(date) DESC ,date ASC LIMIT 3";
+$reNewProduct = pg_query($conn, $sqlNewProduct);
+
 ?>
 <!-- Start Banner Hero -->
 <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
@@ -11,6 +16,7 @@ $reProduct = pg_query($conn, $sqlProduct)
         <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
         <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
     </ol>
+
     <div class="carousel-inner">
         <?php
         $i = 0;
@@ -27,17 +33,11 @@ $reProduct = pg_query($conn, $sqlProduct)
                             </div>
                             <div class="col-lg-6 mb-0 d-flex align-items-center">
                                 <div class="text-align-left align-self-center">
-                                    <h1 class="h1 text-success"><b>ATN</b> Toys strore</h1>
+                                    <h1 class="h1 text-success"><b>ATN</b> Best Products</h1>
                                     <a href="shop-single.php?id=<?= $rowProduct['id'] ?>" class="text-decoration-none text-reset">
-                                    <h3 class="h2"><?= $rowProduct['name'] ?></h3>
+                                        <h3 class="h2"><?= $rowProduct['name'] ?></h3>
                                     </a>
-                                    <p>
-                                        <?= $rowProduct['discription'] ?>
-                                        <!-- <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">TemplateMo</a> website.
-                                        Image credits go to <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
-                                        <a rel="sponsored" class="text-success" href="https://unsplash.com/" target="_blank">Unsplash</a> and
-                                        <a rel="sponsored" class="text-success" href="https://icons8.com/" target="_blank">Icons 8</a>. -->
-                                    </p>
+                                    <p><?= $rowProduct['discription'] ?></p>
                                 </div>
                             </div>
                         </div>
@@ -48,15 +48,15 @@ $reProduct = pg_query($conn, $sqlProduct)
                     <div class="container">
                         <div class="row p-5">
                             <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <a href="shop-single.php?id=<?= $rowProduct['id'] ?>">
-                                <img class="img-fluid" src="./assets/img/<?= $rowProduct['image'] ?>" height="450px" width="450px" alt="img">
-                            </a>
+                                <a href="shop-single.php?id=<?= $rowProduct['id'] ?>">
+                                    <img class="img-fluid" src="./assets/img/<?= $rowProduct['image'] ?>" height="450px" width="450px" alt="img">
+                                </a>
                             </div>
                             <div class="col-lg-6 mb-0 d-flex align-items-center">
                                 <div class="text-align-left">
-                                <a href="shop-single.php?id=<?= $rowProduct['id'] ?>" class="text-decoration-none text-reset">
-                                    <h1 class="h1"><?= $rowProduct['name'] ?></h1>
-                                </a>
+                                    <a href="shop-single.php?id=<?= $rowProduct['id'] ?>" class="text-decoration-none text-reset">
+                                        <h1 class="h1"><?= $rowProduct['name'] ?></h1>
+                                    </a>
                                     <p>
                                         <?= $rowProduct['discription'] ?>
                                     </p>
@@ -79,7 +79,7 @@ $reProduct = pg_query($conn, $sqlProduct)
 
 
 <!-- Start Categories of The Month -->
-<!-- <section class="container py-5">
+<section class="container py-5">
     <div class="row text-center pt-3">
         <div class="col-lg-6 m-auto">
             <h1 class="h1">Categories of The Month</h1>
@@ -106,7 +106,7 @@ $reProduct = pg_query($conn, $sqlProduct)
             <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
         </div>
     </div>
-</section> -->
+</section>
 <!-- End Categories of The Month -->
 
 
@@ -115,86 +115,32 @@ $reProduct = pg_query($conn, $sqlProduct)
     <div class="container py-5">
         <div class="row text-center py-3">
             <div class="col-lg-6 m-auto">
-                <h1 class="h1">Featured Product</h1>
-                <p>
-                    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident.
-                </p>
+                <h1 class="h1">New Products</h1>
+                <p>These are our latest products, you can check them out</p>
             </div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="shop-single.html">
-                        <img src="./assets/img/feature_prod_01.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <ul class="list-unstyled d-flex justify-content-between">
-                            <li>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-muted fa fa-star"></i>
-                                <i class="text-muted fa fa-star"></i>
-                            </li>
-                            <li class="text-muted text-right">$240.00</li>
-                        </ul>
-                        <a href="shop-single.html" class="h2 text-decoration-none text-dark">Gym Weight</a>
-                        <p class="card-text">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                        </p>
-                        <p class="text-muted">Reviews (24)</p>
+            <?php while ($rowNewProduct = pg_fetch_assoc($reNewProduct)) { ?>
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="shop-single.php?id=<?= $rowNewProduct['id'] ?>">
+                            <img src="./assets/img/<?= $rowNewProduct['image'] ?>" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <ul class="list-unstyled d-flex justify-content-between">
+                                <li class="text-muted text-right">Price</li>
+                                <li class="text-muted text-right"><?= $rowNewProduct['sell_price'] ?></li>
+                            </ul>
+                            <a href="shop-single.php?id=<?= $rowNewProduct['id'] ?>" class="h2 text-decoration-none text-dark"><?= $rowNewProduct['name'] ?></a>
+                            <!-- <p class="card-text">
+                                <?php 
+                                // echo $rowNewProduct['discription']; 
+                                ?>
+                            </p> -->
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="shop-single.html">
-                        <img src="./assets/img/feature_prod_02.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <ul class="list-unstyled d-flex justify-content-between">
-                            <li>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-muted fa fa-star"></i>
-                                <i class="text-muted fa fa-star"></i>
-                            </li>
-                            <li class="text-muted text-right">$480.00</li>
-                        </ul>
-                        <a href="shop-single.html" class="h2 text-decoration-none text-dark">Cloud Nike Shoes</a>
-                        <p class="card-text">
-                            Aenean gravida dignissim finibus. Nullam ipsum diam, posuere vitae pharetra sed, commodo ullamcorper.
-                        </p>
-                        <p class="text-muted">Reviews (48)</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <div class="card h-100">
-                    <a href="shop-single.html">
-                        <img src="./assets/img/feature_prod_03.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <ul class="list-unstyled d-flex justify-content-between">
-                            <li>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                                <i class="text-warning fa fa-star"></i>
-                            </li>
-                            <li class="text-muted text-right">$360.00</li>
-                        </ul>
-                        <a href="shop-single.html" class="h2 text-decoration-none text-dark">Summer Addides Shoes</a>
-                        <p class="card-text">
-                            Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec.
-                        </p>
-                        <p class="text-muted">Reviews (74)</p>
-                    </div>
-                </div>
-            </div>
+            <?php } ?>
         </div>
     </div>
 </section>
